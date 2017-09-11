2dabd81df6f98701044dfff64281314f6b7fdb11
Cristian Benescu <cb@project2commerce.com>
2017-09-11 12:11:04 +0300

    Add FeedbackService

---
+++ src/Services/FeedbackService.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Cristian Benescu
+ * Date: 06.09.17
+ * Time: 13:00
+ */
+
+namespace Feedback\Services;
+
+
+use Plenty\Modules\Feedback\Contracts\FeedbackRepositoryContract;
+
+class FeedbackService
+{
+    /**
+     * @param FeedbackRepositoryContract $feedbackRepository
+     * @return \Plenty\Repositories\Models\PaginatedResult
+     */
+    public function listFeedbacks(FeedbackRepositoryContract $feedbackRepository)
+    {
+        return $feedbackRepository->listFeedbacks();
+    }
+}
