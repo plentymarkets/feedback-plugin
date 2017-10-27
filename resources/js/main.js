1c2147105fc8736058a6a9a7c3c8a56a341a2f66
Cristian Benescu <cb@project2commerce.com>
2017-10-27 14:16:18 +0300

    Attributes and repositioned verified purchase + attributes

--- resources/js/main.js
+++ resources/js/main.js
@@ -216,11 +216,11 @@ function feedbackClickLoadMore(feedbackPage){
 // ----------------------------------
 
 function toggleViewReplies(feedbackId){
-    $("#feedback-replies-" + feedbackId).slideToggle();
+    $("#feedback-replies-" + feedbackId).stop().slideToggle();
 }
 
 function toggleAddReply(feedbackId){
-    $("#feedback-add-reply-" + feedbackId).slideToggle();
+    $("#feedback-add-reply-" + feedbackId).stop().slideToggle();
 }
 
 
