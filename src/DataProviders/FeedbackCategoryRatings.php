ef9906604f1961314e4de4ff78493425ef9250b4
Constantin Purcaru <cp@project2commerce.com>
2017-11-06 15:27:18 +0200

    Add provider for displaying the feedback average in the category view items

---
+++ src/DataProviders/FeedbackCategoryRatings.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Constantin Purcaru
+ * Date: 06/11/2017
+ * Time: 15:23
+ */
+
+namespace Feedback\DataProviders;
+
+
+use Plenty\Plugin\Http\Request;
+use Plenty\Plugin\Templates\Twig;
+
+class FeedbackCategoryRatings
+{
+    /**
+     * @param $item
+     * @param Request $request
+     * @param Twig $twig
+     */
+    public function call(Request $request, Twig $twig, $item)
+    {
+
+    }
+}
