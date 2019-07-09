21a41ad9935302ab638c1f836bce95f5441e6796
Felix Gehrmann <felix.gehrmann@plentymarkets.com>
2019-07-09 17:35:43 +0200

    Fix widget position in shopbuilder, add lang strings and rename FeedbachCategoryWidget

---
+++ src/Widgets/FeedbackAverageWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Feedback\Widgets;
+
+use Ceres\Widgets\Helper\BaseWidget;
+
+class FeedbackAverageWidget extends BaseWidget
+{
+    protected $template = "Feedback::Widgets.FeedbackAverageWidget";
+}
