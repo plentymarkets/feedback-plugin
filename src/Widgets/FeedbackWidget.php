5bebbd6ccc2e18d99951413d32c3713437f50961
Felix Gehrmann <felix.gehrmann@plentymarkets.com>
2019-07-19 11:41:19 +0200

    Flatten options object and only pass needed flags

--- src/Widgets/FeedbackWidget.php
+++ src/Widgets/FeedbackWidget.php
@@ -7,4 +7,19 @@ use Ceres\Widgets\Helper\BaseWidget;
 class FeedbackWidget extends BaseWidget
 {
     protected $template = "Feedback::Widgets.FeedbackWidget";
+
+    protected function getTemplateData($widgetSettings, $isPreview)
+    {
+        // As only mobile is currently used, flatten breakpoints
+        return [
+            "options" => [
+                "allowFeedbacksOnlyIfPurchased" => $widgetSettings["allowFeedbacksOnlyIfPurchased"]["mobile"],
+                "allowNoRatingFeedback" => $widgetSettings["allowNoRatingFeedback"]["mobile"],
+                "feedbacksPerPage" => $widgetSettings["feedbacksPerPage"]["mobile"],
+                "numberOfFeedbacks" => $widgetSettings["numberOfFeedbacks"]["mobile"],
+                "releaseFeedbacks" => $widgetSettings["releaseFeedbacks"]["mobile"],
+                "timestampVisibility" => $widgetSettings["timestampVisibility"]["mobile"]
+            ]
+        ];
+    }
 }
