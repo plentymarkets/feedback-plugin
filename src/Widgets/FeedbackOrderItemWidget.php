55fe167c58f8a617dff9b7541fb1934ba2ec0391
Felix Gehrmann <felix.gehrmann@plentymarkets.com>
2019-08-21 18:44:12 +0200

    Set styling and functions for basic use of widget

--- src/Widgets/FeedbackOrderItemWidget.php
+++ src/Widgets/FeedbackOrderItemWidget.php
@@ -3,8 +3,24 @@
 namespace Feedback\Widgets;
 
 use Ceres\Widgets\Helper\BaseWidget;
+use Feedback\Helpers\FeedbackCoreHelper;
 
 class FeedbackOrderItemWidget extends BaseWidget
 {
     protected $template = "Feedback::Widgets.FeedbackOrderItemWidget";
+
+    protected function getTemplateData($widgetSettings, $isPreview)
+    {
+        $allowGuestFeedbacks = pluginApp(FeedbackCoreHelper::class)->configValueAsBool(FeedbackCoreHelper::KEY_ALLOW_GUEST_FEEDBACKS);
+
+        // As only mobile is currently used, flatten breakpoints
+        return [
+            "options" => [
+                "allowNoRatingFeedback" => $widgetSettings["allowNoRatingFeedback"]["mobile"],
+                "allowGuestFeedbacks" => $allowGuestFeedbacks,
+                "itemsPerRow" => $widgetSettings["itemsPerRow"]["mobile"],
+                "rowsPerPage" => $widgetSettings["rowsPerPage"]["mobile"]
+            ]
+        ];
+    }
 }
