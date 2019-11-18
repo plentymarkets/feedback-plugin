ec9208aa304da1fe52745e6c4302d1acd6dfd077
cjohannsen <christoph.johannsen@plentymarkets.com>
2019-11-18 14:15:16 +0100

    ADD RatingFilterWidget

---
+++ src/Widgets/RatingFilterWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Feedback\Widgets;
+
+use Ceres\Widgets\Category\Filter\FilterBaseWidget;
+
+class RatingFilterWidget extends FilterBaseWidget
+{
+    protected $allowedFacetTypes = ["feedback"];
+}
