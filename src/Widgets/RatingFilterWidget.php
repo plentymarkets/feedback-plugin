7567674cfead93c2002066320a8405c71baa3f27
cjohannsen <christoph.johannsen@plentymarkets.com>
2019-11-18 17:26:22 +0100

    CHANGE RatingFilterWidget.php - add fake data

--- src/Widgets/RatingFilterWidget.php
+++ src/Widgets/RatingFilterWidget.php
@@ -3,8 +3,40 @@
 namespace Feedback\Widgets;
 
 use Ceres\Widgets\Category\Filter\FilterBaseWidget;
+use IO\Services\ItemSearch\Factories\Faker\Traits\FakeNumber;
+use IO\Services\ItemSearch\Factories\Faker\Traits\FakeString;
 
 class RatingFilterWidget extends FilterBaseWidget
 {
+    use FakeNumber, FakeString;
+
     protected $allowedFacetTypes = ["feedback"];
+
+    protected function getPreviewData($widgetSettings)
+    {
+        $feedback = [
+            "id" => "feedback",
+            "name" => $this->trans("Feedback::Widget.feedbackLabel"),
+            "position" => 0,
+            "values" => [],
+            "minHitCount" => 1,
+            "maxResultCount" => 5
+        ];
+
+        for ($i = 1; $i <= 5; $i++)
+        {
+            $feedback["values"][] =
+            [
+                "id" => "feedback-" . $i,
+                "names" =>
+                [
+                    ["lang" => "de", "name" => ""]
+                ],
+                "count" => $this->number(3, 20),
+            ];
+        }
+        $feedback["count"] = 5;
+
+        return [ "facets" => [ $feedback ] ];
+    }
 }
