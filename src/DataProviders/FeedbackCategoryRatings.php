04494b432830a2646901d1185d623db8f1d538b9
cerwantes <max1162@web.de>
2022-12-14 09:05:30 +0100

    added option to show rating count in category view

--- src/DataProviders/FeedbackCategoryRatings.php
+++ src/DataProviders/FeedbackCategoryRatings.php
@@ -26,14 +26,18 @@ class FeedbackCategoryRatings
 
         if( empty($average)) {
             $counts['averageValue'] = 0;
+            $counts['ratingsCountTotal'] = 0;
         } else {
             $counts['averageValue'] = $average->averageValue;
+            $counts['ratingsCountTotal'] = $average->ratingsCountTotal;
         }
 
         $data['counts'] = $counts;
 
         $showEmptyRatingsInCategoryView = $coreHelper->configValueAsBool(FeedbackCoreHelper::KEY_SHOW_EMPTY_RATINGS_IN_CATEGORY_VIEW);
+        $showRatingCountInCategoryView = $coreHelper->configValueAsBool(FeedbackCoreHelper::KEY_SHOW_RATING_COUNT_IN_CATEGORY_VIEW);
         $data['options']['showEmptyRatingsInCategoryView'] = $showEmptyRatingsInCategoryView;
+        $data['options']['showRatingCountInCategoryView'] = $showRatingCountInCategoryView;
 
         return $twig->render('Feedback::DataProvider.CategoryAverageRating', $data);
     }
--- src/Helpers/FeedbackCoreHelper.php
+++ src/Helpers/FeedbackCoreHelper.php
@@ -11,6 +11,7 @@ class FeedbackCoreHelper
     const KEY_RELEASE_FEEDBACKS_AUTOMATICALLY = "autoreleaseFeedbacks";
     const KEY_ALLOW_GUEST_FEEDBACKS = "allowGuestFeedbacks";
     const KEY_SHOW_EMPTY_RATINGS_IN_CATEGORY_VIEW = "showEmptyRatingsInCategoryView";
+    const KEY_SHOW_RATING_COUNT_IN_CATEGORY_VIEW = "showRatingCountInCategoryView";
     const KEY_SHOW_RATING_SORTING = "showRatingSorting";
     const KEY_SHOW_RATING_FACET = "showRatingFacet";
     const KEY_NUMBER_OF_FEEDBACKS = "numberOfFeedbacks";
