ebf45b57f57ed9535a703bb39dab8b134a53f777
R. Teodor <teodor.rosu@plentymarkets.com>
2024-02-27 12:04:22 +0200

    test

--- src/Services/FeedbackService.php
+++ src/Services/FeedbackService.php
@@ -208,12 +208,12 @@ class FeedbackService
 
             return $result;
         } elseif ($this->request->input('type') === 'reply') {
-            $feedbackId     = (int) $options['feedbackRelationTargetId'];
-            $feedbackExists = $this->feedbackExists($feedbackId);
-
-            if (!$feedbackExists) {
-                return 'Feedback does not exist.';
-            }
+//            $feedbackId     = (int) $options['feedbackRelationTargetId'];
+//            $feedbackExists = $this->feedbackExists($feedbackId);
+//
+//            if (!$feedbackExists) {
+//                return 'Feedback does not exist.';
+//            }
 
             $options['feedbackRelationTargetType'] = 'feedback';
             $options['isVisible'] = $this->determineVisibility($autoreleaseFeedbacks, $creatorContactId);
