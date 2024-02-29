03320cb44b767920848aa1dba54085e3c1de7240
R. Teodor <teodor.rosu@plentymarkets.com>
2024-02-29 15:41:33 +0200

    - Fix database content not updating after create procedure. Adding pluginApp to fix it.

--- src/Services/FeedbackService.php
+++ src/Services/FeedbackService.php
@@ -209,11 +209,11 @@ class FeedbackService
             return $result;
         } elseif ($this->request->input('type') === 'reply') {
             $feedbackId     = (int) $options['feedbackRelationTargetId'];
-//            $feedbackExists = $this->feedbackExists($feedbackId);
-//
-//            if (!$feedbackExists) {
-//                return 'Feedback does not exist.';
-//            }
+            $feedbackExists = $this->feedbackExists($feedbackId);
+
+            if (!$feedbackExists) {
+                return 'Feedback does not exist.';
+            }
 
             $options['feedbackRelationTargetType'] = 'feedback';
             $options['isVisible'] = $this->determineVisibility($autoreleaseFeedbacks, $creatorContactId);
@@ -667,9 +667,10 @@ class FeedbackService
     private function feedbackExists(int $feedbackId): bool
     {
         try {
-            /** @var Feedback $feedback */
-            $feedback = $this->feedbackRepository->getFeedback($feedbackId);
-        } catch (\Throwable $exception) {}
+            /** @var FeedbackRepositoryContract $feedbackRepository */
+            $feedbackRepository = pluginApp(FeedbackRepositoryContract::class);
+            $feedback           = $feedbackRepository->getFeedback($feedbackId);
+        } catch (ModelNotFoundException $exception) {}
 
         $this->getLogger(__METHOD__)->debug('Feedback::Debug.feedbackExistsResult', [
             'expectedFeedbackId' => $feedbackId,
