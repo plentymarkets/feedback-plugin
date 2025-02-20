b4aa4598cb9ea9197eeafbb272cb9c6e439cb8f8
plentydev <iulian.vladu@plentymarkets.com>
2025-02-20 15:15:44 +0200

    build 21

--- resources/js/src/app/components/item/singleitem/FeedbackListEntry.vue
+++ resources/js/src/app/components/item/singleitem/FeedbackListEntry.vue
@@ -1,5 +1,6 @@
 <template>
   <div
+    v-if="(showControls && !feedbackData.isVisible) || (!showControls && feedbackData.isVisible)"
     class="feedback clearfix"
     :class="{'loading':isLoading}"
   >
